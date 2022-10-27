49e8dee1d1881624ee188895e0494b8cf4aa1f6e
Jay Goyal <f20212418@pilani.bits-pilani.ac.in>
2022-10-27 22:09:56 +0530

    Finalised Student Class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,87 +2,124 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Student implements User {
-	class Wash {
+public class Student extends User {
+	public class Wash {
 		private final double weight;
 		private LocalDate dateGiven;
 		private boolean isReady;
 		private LocalDate dateRecvd;
 
-		public Wash(double weight) {
+		private Wash(double weight) {
 			this.weight = weight;
 			this.dateGiven = LocalDate.now();
 			this.isReady = false;
 			this.dateRecvd = null;
 		}
 
-		public Wash(double weight, LocalDate dateGiven) {
+		private Wash(double weight, LocalDate dateGiven) {
 			this.weight = weight;
 			this.dateGiven = dateGiven;
 			this.isReady = false;
 			this.dateRecvd = null;
 		}
+
+		private void setDateRecvd(LocalDate date) {
+			dateRecvd = date;
+		}
+
+		public LocalDate getDateGiven() {
+			return dateGiven;
+		}
 	}
 
-	class Plan {
+	public class Plan {
 		private final boolean isIron;
 		private final int numWashes;
 		private final double costPerWash;
 		private int washesGiven;
 		private boolean isFinished;
 		private boolean isWashGiven;
+		private boolean isWashBack;
 		private Wash[] washes;
 
-		public Plan(boolean isIron, int numWashes) {
+		private Plan(boolean isIron, int numWashes) {
 			this.isIron = isIron;
 			this.numWashes = numWashes;
 			this.washesGiven = 0;
 			this.washes = new Wash[numWashes];
 			this.isFinished = false;
+			this.isWashGiven = false;
+			this.isWashBack = false;
 			this.costPerWash = 220;
 		}
 
-		public Plan(boolean isIron, int numWashes, double costPerWash) {
+		private Plan(boolean isIron, int numWashes, double costPerWash) {
 			this.isIron = isIron;
 			this.numWashes = numWashes;
 			this.washesGiven = 0;
 			this.washes = new Wash[numWashes];
 			this.isFinished = false;
+			this.isWashGiven = false;
+			this.isWashBack = false;
 			this.costPerWash = costPerWash;
 		}
 
-		public void addWash(Wash wash) {
+		private void addWash(Wash wash) {
 			if (washesGiven == numWashes) {
 				return;
 			}
+			isWashGiven = true;
+			isWashBack = false;
 			washes[washesGiven] = wash;
 			washesGiven++;
 		}
 
 		public void returnWash() {
 			isWashGiven = false;
+			isWashBack = true;
 		}
 
-		public boolean getIsWashGiven() {
+		public int getWashesGiven() {
+			return washesGiven;
+		}
+
+		private void recvWash() {
+			washes[washesGiven].setDateRecvd(LocalDate.now());
+		}
+
+		private boolean getIsWashGiven() {
 			return isWashGiven;
 		}
 
-		public boolean getIsFinished() {
+		private boolean getIsWashBack() {
+			return isWashBack;
+		}
+
+		private boolean getIsFinished() {
 			if (washesGiven == numWashes) {
 				isFinished = true;
 			}
 			return isFinished;
 		}
 
-		public boolean getIsIron() {
+		private boolean getIsIron() {
 			return isIron;
 		}
 
-		public double getPlanCost() {
+		private double getCostPerWash() {
+			return costPerWash;
+		}
+
+		private double getPlanCost() {
 			return costPerWash * numWashes;
+		}
+
+		private Wash getLatestWash() {
+			return washes[washesGiven];
 		}
 	}
 
+	public static final int WEIGHT_LIMIT = 6;
 	public final String userName;
 	public Hostel hostel;
 	private ArrayList<Plan> plans = new ArrayList<Plan>();
@@ -98,49 +135,53 @@ public class Student implements User {
 		this.hostel = hostel;
 	}
 
-	public boolean checkLogin(String testPass) {
-		if (testPass == password) {
-			System.out.println(fullName + " has logged in.");
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public static Student register(String userName, String fullName, String password, String secretWord,
-			Hostel hostel) {
-		return new Student(
+	public static Student register(String userName, String fullName, String password, String secretWord, Hostel hostel,
+			WashPlan washPlan) {
+		Student student = new Student(
 				userName,
 				fullName,
 				password,
 				secretWord,
 				hostel);
+		student.newPlan(washPlan);
+		return student;
 	}
 
-	public void resetPassword(String testWord) {
-		Scanner sc = new Scanner(System.in);
-		if (testWord.equals(secretWord)) {
-			password = sc.nextLine();
-			System.out.println("Password Reset");
-		}
-		sc.close();
+	public ArrayList<Plan> getPlans() {
+		return plans;
 	}
 
-	public void newPlan(boolean isIron, int numWashes) {
-		plans.add(new Plan(isIron, numWashes));
+	public void setPlans(boolean isIron, int numWashes, double costPerWash) {
+		plans.set(plans.size() - 1, new Plan(isIron, numWashes, costPerWash)); 
 	}
 
-	public void newPlan(boolean isIron, int numWashes, double cost) {
+	public void newPlan(WashPlan plan) {
+		plans.add(new Plan(plan.isIron, plan.numWashes, plan.costPerWash));
+	}
+
+	private void newPlan(boolean isIron, int numWashes, double cost) {
 		plans.add(new Plan(isIron, numWashes, cost));
 	}
 
 	public void giveWash(double weight) {
 		Plan activePlan = plans.get(plans.size() - 1);
+		if (activePlan.getIsFinished()) {
+			System.out
+					.println("Plan has been exceeded. 50Rs penalty will be charged. Get new plan to avoid in future.");
+			newPlan(activePlan.getIsIron(), 1, activePlan.getCostPerWash() + 50);
+			activePlan = plans.get(plans.size() - 1);
+		}
 		if (activePlan.getIsWashGiven()) {
 			System.out.println("Wash already given");
 			return;
 		}
-		plans.get(plans.size() - 1).addWash(new Wash(weight));
+		while (weight > 0) {
+			activePlan.addWash(new Wash(weight));
+			weight = weight - WEIGHT_LIMIT;
+			if (weight < 0) {
+				weight = 0;
+			}
+		}
 	}
 
 	public void giveWash(double weight, LocalDate date) {
@@ -148,14 +189,29 @@ public class Student implements User {
 		if (activePlan.getIsFinished()) {
 			System.out
 					.println("Plan has been exceeded. 50Rs penalty will be charged. Get new plan to avoid in future.");
-			newPlan(activePlan.getIsIron(), 1, 270);
+			newPlan(activePlan.getIsIron(), 1, activePlan.getCostPerWash() + 50);
 			activePlan = plans.get(plans.size() - 1);
 		}
 		if (activePlan.getIsWashGiven()) {
 			System.out.println("Wash already given");
 			return;
 		}
-		activePlan.addWash(new Wash(weight, date));
+		while (weight > 0) {
+			activePlan.addWash(new Wash(weight, date));
+			weight = weight - WEIGHT_LIMIT;
+			if (weight < 0) {
+				weight = 0;
+			}
+		}
+	}
+
+	public void recvWash() {
+		Plan activePlan = plans.get(plans.size() - 1);
+		if (!activePlan.getIsWashGiven()) {
+			System.out.println("No wash given");
+			return;
+		}
+		activePlan.recvWash();
 	}
 
 	public double cost() {
@@ -164,5 +220,9 @@ public class Student implements User {
 			totalCost += plan.getPlanCost();
 		}
 		return totalCost;
+	}
+
+	public Wash getLastWash() {
+		return plans.get(plans.size() - 1).getLatestWash();
 	}
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,8 +1,34 @@
-public interface User {
+import java.util.Scanner;
+
+interface UserFields {
 	final String userName = "";
 	String fullName = "";
 	String password = "";
 	String secretWord = "";
 
-	boolean checkLogin(String testPass);
+}
+
+public abstract class User implements UserFields {
+	public final String userName = "";
+	private String fullName = "";
+	private String password = "";
+	private String secretWord = "";
+
+	public boolean checkLogin(String testPass) {
+		if (testPass == password) {
+			System.out.println(fullName + " has logged in.");
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void resetPassword(String testWord) {
+		Scanner sc = new Scanner(System.in);
+		if (testWord.equals(secretWord)) {
+			password = sc.nextLine();
+			System.out.println("Password Reset");
+		}
+		sc.close();
+	}
 }
